818bba09865cc29fade64043d3a1ba687de6721e
sfalexrog <sfalexrog@gmail.com>
2015-11-19 00:20:12 +0300

    Initial commit for OpenApoc for Android.

---
+++ OpenApoc/src/main/java/org/sfalexrog/openapoc/OpenApocActivity.java
@@ -0,0 +1,13 @@
+package org.sfalexrog.openapoc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpenApocActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_apoc);
+    }
+}
